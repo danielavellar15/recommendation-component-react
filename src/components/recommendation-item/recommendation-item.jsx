062dd2c4d3260c7eee9deb3132cbe6f0bdbce7a2
danielavellar15@hotmail.com <danielavellar15@hotmail.com>
2021-02-05 17:56:51 -0300

    first commit

---
+++ src/components/recommendation-item/recommendation-item.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './recommendation-item.css'
+
+export default function RecommendationItem(props) {
+
+}
---
+++ src/components/recommendation-list/recommendation-list.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './recommendation-list.css'
+
+export default function RecommendationList(props) {
+
+}
